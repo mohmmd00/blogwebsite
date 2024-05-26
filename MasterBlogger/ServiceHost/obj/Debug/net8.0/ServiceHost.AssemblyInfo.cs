3863b3c397b140b7152e682b217c3bd38366162f
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2024-05-26 21:29:40 +0330

    Reason -> Fixed the null reference problem on the create ArticleCategory page

--- MasterBlogger/ServiceHost/obj/Debug/net8.0/ServiceHost.AssemblyInfo.cs
+++ MasterBlogger/ServiceHost/obj/Debug/net8.0/ServiceHost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaae3f836228da8a91c1cdeb80d1c9abd15f0eca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad4428c90e8f2d8725899120e1692bed2563c48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
